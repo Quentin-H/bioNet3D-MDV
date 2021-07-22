5df720de7bc303b674f7ae48e486d5c687b2af97
Quentin H <qjherzig@gmail.com>
2021-07-22 17:43:13 -0500

    Added sprite offset + Generated output file for human genome

--- Pre-Processing Project/PyPreProcessing/PyPreProcessing.py
+++ Pre-Processing Project/PyPreProcessing/PyPreProcessing.py
@@ -2,7 +2,7 @@ import sys
 import igraph
 
 print('Python version ', sys.version)
-print('MDV PreProcessor Version 0.7')
+print('MDV PreProcessor Version 0.9')
 print(' ')
 print(' ')  # E:\Quentin\Github Repositories\bioNet3D-MDV\Sample Files\9606.reactome_PPI_reaction.edge
 
@@ -39,31 +39,31 @@ graph = igraph.Graph(
         "Edge_Weight": 0
     })
 
-i = 1
-for nodeLine in nodeFileLines:
+i = 1 # first line has headers
+for nodeLine in nodeFileLines: # go through every gene in the file and add it as a node to the graph
     featureID = nodeLine.split()[0]
     dName = nodeLine.split()[3]
     desc = nodeLine.split("\t")[4]
     nRank = i
     bScore = 0
 
-    for scoreLine in scoreFileLines:
-        if scoreLine.split()[1] == featureID:
-            bScore = scoreLine.split()[4]
-            break
+    #for scoreLine in scoreFileLines: # searches for the baseline score in the score file
+    #    if scoreLine.split()[1] == featureID: 
+    #        bScore = scoreLine.split()[4]
+    #        break
     
     # Sets the name of the vertex as the knowENG ID, this lets us refer to the vertex by ID rather than index, has one attribute
     graph.add_vertex(name = featureID, displayName = dName, description = desc, networkRank = nRank, baselineScore = bScore)
     i += 1
 
-# go through the edge input file for each edge
+# go through the edge input file for each edge and create an edge between both genes (which are nodes in the network due to the previous step)
 for edgeLine in edgeFileLines:
     node1 = edgeLine.split()[0]
     node2 = edgeLine.split()[1]
     weight = edgeLine.split()[2]
     graph.add_edge(node1, node2, Edge_Weight = weight)
 
-# Runs the inputted layout algorythm 
+# Runs the layout algorythm the user chose earlier
 graphLayout = graph.layout(graphOption)
 
 # Converts the layout object to a string 
@@ -86,5 +86,5 @@ for coordinate in graphLayout:
 
 # Saves the string we created as a "massive dataset visualizer layout file"
 outputFile = open(outputPath + ("output - " + graphOption + ".mdvl"), "w")
-outputFile.write(layoutString) # change this to write to binary
+outputFile.write(layoutString)
 outputFile.close
--- Unity Project/Assets/CustomBillboardSystem/Billboard.cs
+++ Unity Project/Assets/CustomBillboardSystem/Billboard.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public float scale = 1.0f;
+    public float yawAngleOffset = 0.0f;
     //public float maxScale = 500.0f;
     private Camera camera;
     private Vector3 initialScale; 
@@ -19,6 +20,7 @@ public class Billboard : MonoBehaviour
     private void Update()
     {
         transform.LookAt(camera.transform);
+        transform.Rotate(0f, 0f, yawAngleOffset);
 
         Plane plane = new Plane(camera.transform.forward, camera.transform.position); 
 		float dist = plane.GetDistanceToPoint(transform.position);
