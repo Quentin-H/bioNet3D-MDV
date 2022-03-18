3a25959fd28180801c7195b54742bda4321113d9
Quentin Herzig <qherzig2@illinois.edu>
2022-03-17 21:57:22 -0500

    fixed log scaling

--- Pre-Processing Project/PyPreProcessing/Functions.py
+++ Pre-Processing Project/PyPreProcessing/Functions.py
@@ -242,7 +242,7 @@ class Functions:
 			j = 0
 			for coordinate in layout: # for each node in the graph
 				newGraph.vs[j]["cluster"] = i + 1
-				coord = [coordinate[0], coordinate[1], 5 + math.log(newGraph.vs[j].degree()) ]
+				coord = [coordinate[0], coordinate[1], math.log(newGraph.vs[j].degree() + 1) ]
 				coord = numpy.dot( coord, tfmrot ) + tfmtranslate
 				
 				coord = "[%g,%g,%g]" % (coord[0], coord[1], coord[2])
